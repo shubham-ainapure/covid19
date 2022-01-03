876aa21fc85c680f1778f0bda70f7b04154dbd36
shubham-ainapure <56270447+shubham-ainapure@users.noreply.github.com>
2022-01-03 17:38:48 +0530

    final

--- covid.js
+++ covid.js
@@ -1,20 +1,20 @@
- fetch("https://api.covid19api.com/summary")
+ fetch("https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true")
 .then((data) =>{
- 	return data.json();
+  return data.json();
  })
 .then((coviddata) =>{
- 	const data=coviddata.Countries[76];
-    let total=data.TotalConfirmed;
-    let death=data.TotalDeaths;
-    let recover=total-death;
+  const data=coviddata;
+    let total=data.totalCases;
+    let death=data.deaths;
+    // let recover=total-death;
     // const recovered=data.TotalConfirmed-data.TotalDeaths;
- 	document.getElementById("total").innerHTML=total;
+  document.getElementById("total").innerHTML=total;
     document.getElementById("death").innerHTML=death;
-    document.getElementById("new").innerHTML=data.NewConfirmed;
-    document.getElementById("recovered").innerHTML=recover;
- 	// const data=coviddata;
- 	console.log(coviddata);
+    document.getElementById("new").innerHTML=data.activeCases;
+    document.getElementById("recovered").innerHTML=data.recovered;
+  // const data=coviddata;
+  console.log(coviddata);
  })
 .catch((error) =>{
- 	console.log("failed to fetch data");
+  console.log("failed to fetch data");
  })
